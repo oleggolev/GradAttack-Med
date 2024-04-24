02700386fe4b4a0590d6a7f89c74a543dbfb3bdc
Oleg Golev <ogolev@princeton.edu>
2024-04-24 10:23:40 -0400

    add run_train.sh

---
+++ run_train.sh
@@ -0,0 +1,6 @@
+sbatch scripts/train/insta.slurm
+sbatch scripts/train/mixup.slurm
+sbatch scripts/train/prune-insta.slurm
+sbatch scripts/train/prune-mixup.slurm
+sbatch scripts/train/prune.slurm
+sbatch scripts/train/vanilla.slurm
