53b25a67b49b27b2a8623d2cdc415424ee11fa04
Oleg Golev <ogolev@princeton.edu>
2024-04-24 02:32:01 -0400

    update env and slurm scripts

--- make_venv.sh
+++ make_venv.sh
@@ -3,5 +3,5 @@ mkdir grad-attack-env
 virtualenv grad-attack-env
 source grad-attack-env/bin/activate
 python3 -m pip install -r requirements.txt
-pip3 install torch==1.11.0+cu113 --extra-index-url https://download.pytorch.org/whl/cu113
+pip install torch==1.8.1+cu111 torchvision==0.9.1+cu111 torchaudio===0.8.1 -f https://download.pytorch.org/whl/torch_stable.html
 deactivate
