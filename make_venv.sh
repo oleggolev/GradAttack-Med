f1e664ac9acf9ced6f3d4303776fb80e591de844
Oleg Golev <ogolev@princeton.edu>
2024-04-24 01:50:26 -0400

    fix A100 incompatibility with torch

--- make_venv.sh
+++ make_venv.sh
@@ -3,4 +3,5 @@ mkdir grad-attack-env
 virtualenv grad-attack-env
 source grad-attack-env/bin/activate
 python3 -m pip install -r requirements.txt
+pip3 install torch==1.11.0+cu113 --extra-index-url https://download.pytorch.org/whl/cu113
 deactivate
