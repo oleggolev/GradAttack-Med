d176553aa11bb238723c6a93c4fb74be122c4454
Oleg Golev <ogolev@princeton.edu>
2024-05-06 20:23:29 -0400

    run only no-defenses

--- scripts/Brain-Tumor-MRI/run_attack.sh
+++ scripts/Brain-Tumor-MRI/run_attack.sh
@@ -12,48 +12,48 @@ scommand="sbatch -J attack-no-defenses-${batch_size} attack/vanilla.slurm $batch
 echo "submit command: $scommand"
 $scommand
 
-# Attack with GradPrune.
-for p in 0.5 0.7 0.9 0.95 0.99 0.999
-do
-    scommand="sbatch -J attack-prune-${p}-${batch_size} attack/prune.slurm $p $batch_size"
-    echo "submit command: $scommand"
-    $scommand
-done
+# # Attack with GradPrune.
+# for p in 0.5 0.7 0.9 0.95 0.99 0.999
+# do
+#     scommand="sbatch -J attack-prune-${p}-${batch_size} attack/prune.slurm $p $batch_size"
+#     echo "submit command: $scommand"
+#     $scommand
+# done
 
-# Attack with MixUp.
-for k in 2 4 6 8
-do
-    scommand="sbatch -J attack-mixup-${k}-${batch_size} attack/mixup.slurm $k $batch_size"
-    echo "submit command: $scommand"
-    $scommand
-done
+# # Attack with MixUp.
+# for k in 2 4 6 8
+# do
+#     scommand="sbatch -J attack-mixup-${k}-${batch_size} attack/mixup.slurm $k $batch_size"
+#     echo "submit command: $scommand"
+#     $scommand
+# done
 
-# Attack with InstaHide.
-for k in 2 4 6 8
-do
-    scommand="sbatch -J attack-insta-${k}-${batch_size} attack/insta.slurm $k $batch_size"
-    echo "submit command: $scommand"
-    $scommand
-done
+# # Attack with InstaHide.
+# for k in 2 4 6 8
+# do
+#     scommand="sbatch -J attack-insta-${k}-${batch_size} attack/insta.slurm $k $batch_size"
+#     echo "submit command: $scommand"
+#     $scommand
+# done
 
-# Attack with MixUp + GradPrune.
-for k in 2 4 6
-do
-    for p in 0.7 0.9 0.99
-    do
-        scommand="sbatch -J attack-prune-mixup-${p}-${k}-${batch_size} attack/prune-mixup.slurm $p $k $batch_size"
-        echo "submit command: $scommand"
-        $scommand
-    done
-done
+# # Attack with MixUp + GradPrune.
+# for k in 2 4 6
+# do
+#     for p in 0.7 0.9 0.99
+#     do
+#         scommand="sbatch -J attack-prune-mixup-${p}-${k}-${batch_size} attack/prune-mixup.slurm $p $k $batch_size"
+#         echo "submit command: $scommand"
+#         $scommand
+#     done
+# done
 
-# Attack with InstaHide + GradPrune.
-for k in 2 4 6
-do
-    for p in 0.7 0.9 0.99
-    do
-        scommand="sbatch -J attack-prune-insta-${p}-${k}-${batch_size} attack/prune-insta.slurm $p $k $batch_size"
-        echo "submit command: $scommand"
-        $scommand
-    done
-done
+# # Attack with InstaHide + GradPrune.
+# for k in 2 4 6
+# do
+#     for p in 0.7 0.9 0.99
+#     do
+#         scommand="sbatch -J attack-prune-insta-${p}-${k}-${batch_size} attack/prune-insta.slurm $p $k $batch_size"
+#         echo "submit command: $scommand"
+#         $scommand
+#     done
+# done
